31fca2b31cfe3347d006e09060801b6b3726808f
Waldek <sp2ong@gmail.com>
2026-09-20 09:13:36 +0200

    update

--- index.php
+++ index.php
@@ -20,8 +20,14 @@ $isInternalClient = is_internal_client(load_allowed_networks($config));
         <meta name="description" content="<?php echo htmlspecialchars($nodeTitle); ?> - AllStarLink Ham Radio Control Panel">
         <meta name="robots" content="noindex, nofollow">
         <meta name="author" content="M0NFI, G7RPG, G4IYT, SP2ONG">
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-        <link rel="icon" href="favicon.ico" type="image/x-icon">
+        <!-- Favicons -->
+	<link rel="apple-touch-icon" href="img/favicons/favicon-180x180.png" type="image/png" sizes="180x180">
+	<link rel="icon" href="img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+	<link rel="icon" href="img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+	<link rel="icon" href="img/favicons/favicon-192x192.png" sizes="192x192" type="image/png">
+	<link rel="icon" href="img/favicons/favicon-512x512.png" sizes="512x512" type="image/png">
+	<link rel="icon" href="img/favicons/favicon.ico" type="image/png">
+
         <link rel="stylesheet" href="css/dashboard-live.css">
     </head>
     <body>
